0ffb53e733915cec8f947d7f1056ba96c7109790
rizky putra <rizkyputra531.rp@gmail.com>
2023-12-06 19:40:13 +0800

    Make Migration : Kas Keluark

---
+++ database/migrations/2023_12_06_113906_create_kas_keluar_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kas_keluar', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->nullable();
+
+            
+            $table->string('jenis_pemasukan')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->date('tanggal')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->integer('harga')->nullable();
+            $table->integer('total')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kas_keluar');
+    }
+};
